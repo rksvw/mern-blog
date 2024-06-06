1f0d0c5200c5fed8b212ea23c70aa8e08ace0dcf
rksvw <ritik.vw98@gmail.com>
2024-06-06 19:42:07 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("mongodb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
