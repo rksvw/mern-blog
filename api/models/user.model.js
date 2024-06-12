2973df21055398129848ca39262ad7cbbc389ed2
rksvw <ritik.vw98@gmail.com>
2024-06-12 09:02:20 +0530

    make the dashboard private and prettier the model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -18,12 +18,13 @@ const userSchema = new mongoose.Schema(
     },
     profilePhoto: {
       type: String,
-      default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
-    }
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    },
   },
   { timestamps: true }
 );
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
