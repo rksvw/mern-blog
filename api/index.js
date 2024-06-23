0b6ac50e56beeb95ed8713cea02a800d674c82d0
Ritik Sharma <156570972+rksvw@users.noreply.github.com>
2024-06-23 09:33:48 +0530

    add create a post api route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require('dotenv').config();
-const userRoutes = require('./routes/user.route');
-const authRoutes = require('./routes/auth.route');
-const cookieParser = require('cookie-parser');
+require("dotenv").config();
+const userRoutes = require("./routes/user.route");
+const authRoutes = require("./routes/auth.route");
+const cookieParser = require("cookie-parser");
+const postRoutes = require("./routes/post.route");
 
 mongoose
   .connect(process.env.MONGO)
@@ -20,21 +21,21 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
-
 // MIDDLEWARE
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   res.status(statusCode).json({
     success: false,
     statusCode,
     message,
-  })
+  });
 });
---
+++ api/routes/post.route.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const { verifyToken } = require("../utils/verifyUser");
+const { create } = require("../controllers/post.controller");
+const router = express.Router();
+
+module.exports = router.post("/create-post", verifyToken, create);
