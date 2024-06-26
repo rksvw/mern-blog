1a072acfb67e45cbe039276bb64667fe9d18111e
Ritik Sharma <156570972+rksvw@users.noreply.github.com>
2024-06-26 10:03:43 +0530

    add post section to the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import DashSidebar from "../components/DashSidebar";
 import DashProfile from "../components/DashProfile";
+import DashPosts from "../components/DashPosts";
 
 export default function Dashboard() {
   const location = useLocation();
@@ -16,9 +17,13 @@ export default function Dashboard() {
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
       <div className="md:w-56">
+        {/* Sidebar */}
         <DashSidebar />
       </div>
+      {/* Profile */}
       {tab === "profile" && <DashProfile />}
+      {/* Posts */}
+      {tab === "posts" && <DashPosts />}
     </div>
   );
 }
