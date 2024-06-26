239c0860127cd25bf9d99bfebcb32aea836d923e
Ritik Sharma <156570972+rksvw@users.noreply.github.com>
2024-06-26 12:41:35 +0530

    create get post api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { verifyToken } = require("../utils/verifyUser");
-const { create } = require("../controllers/post.controller");
+const { create, getPosts } = require("../controllers/post.controller");
 const router = express.Router();
 
 module.exports = router.post("/create", verifyToken, create);
+module.exports = router.get("/getposts", getPosts);
