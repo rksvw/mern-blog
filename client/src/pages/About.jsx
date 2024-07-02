5e2a9974a25f6c135a750545f0a74898ca375eec
Ritik Sharma <156570972+rksvw@users.noreply.github.com>
2024-07-02 09:37:43 +0530

    complete about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About MERN Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p className="">
+              Mern Blog is a blog that I created to share my thoughts and ideas
+              with the world. I am a software engineer and I love to write about
+              my experiences and things that I have learned. I hope you enjoy
+              reading my blog.
+            </p>
+            <p className="">
+              On this blog, you&apos;ll find weekly articles and tutorials on
+              topics such as web development, software engineering, and
+              programming languages. Sahand is always learning and exploring new
+              technologies, so be sure to check back often for new content!
+            </p>
+            <p className="">
+              We encourage you to leave comments on our posts and engage with
+              other readers. You can like other people&apos;s comments and reply
+              to them as well. We believe that a community of learners can help
+              each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
