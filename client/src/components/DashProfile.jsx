26a83fe0464ddf03ea4cdacef46c6e61b0d64692
Ritik Sharma <156570972+rksvw@users.noreply.github.com>
2024-07-22 22:55:20 +0530

    profile picture update no reloading problem fixed

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -88,56 +88,57 @@ export default function DashProfile() {
 
   useEffect(() => {
     if (imageFile) {
-      const uploadImage = async () => {
-        // Craft rules based on data in your Firestore database
-        // allow write: if firestore.get(
-        //    /databases/(default)/documents/users/$(request.auth.uid)).data.isAdmin;
-        // service firebase.storage {
-        //   match /b/{bucket}/o {
-        //     match /{allPaths=**} {
-        //       allow read, write: if
-        //       request.resource.size < 2 * 1024 * 1024 &&
-        //       request.resource.contentType.matches('image/.*')
-        //     }
-        //   }
-        // }
-
-        setImageFileUploading(true);
-        setImageFileUploadError(null);
-        const storage = getStorage(app);
-        const fileName = new Date().getTime() + imageFile.name;
-        const storageRef = ref(storage, fileName);
-        const uploadTask = uploadBytesResumable(storageRef, imageFile);
-        uploadTask.on(
-          "state_changed",
-          (snapshot) => {
-            const progress =
-              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-
-            setImageFileUploadProgress(progress.toFixed(0));
-          },
-          // eslint-disable-next-line no-unused-vars
-          (error) => {
-            setImageFileUploadError(
-              "Could not upload image (File must be less than 2MB)"
-            );
-            setImageFileUploadProgress(null);
-            setImageFile(null);
-            setImageFileUrl(null);
-            setImageFileUploading(false);
-          },
-          () => {
-            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-              setImageFileUrl(downloadURL);
-              setFormData({ ...formData, profilePhoto: downloadURL });
-              setImageFileUploading(false);
-            });
-          }
-        );
-      };
       uploadImage();
     }
-  }, [imageFile, formData]);
+  }, [imageFile]);
+
+  const uploadImage = async () => {
+    // Craft rules based on data in your Firestore database
+    // allow write: if firestore.get(
+    //    /databases/(default)/documents/users/$(request.auth.uid)).data.isAdmin;
+    // service firebase.storage {
+    //   match /b/{bucket}/o {
+    //     match /{allPaths=**} {
+    //       allow read, write: if
+    //       request.resource.size < 2 * 1024 * 1024 &&
+    //       request.resource.contentType.matches('image/.*')
+    //     }
+    //   }
+    // }
+
+    setImageFileUploading(true);
+    setImageFileUploadError(null);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+        setImageFileUploadProgress(progress.toFixed(0));
+      },
+      // eslint-disable-next-line no-unused-vars
+      (error) => {
+        setImageFileUploadError(
+          "Could not upload image (File must be less than 2MB)"
+        );
+        setImageFileUploadProgress(null);
+        setImageFile(null);
+        setImageFileUrl(null);
+        setImageFileUploading(false);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL);
+          setFormData({ ...formData, profilePhoto: downloadURL });
+          setImageFileUploading(false);
+        });
+      }
+    );
+  };
 
   const handleDeleteUser = async () => {
     setShowModal(false);
@@ -178,7 +179,7 @@ export default function DashProfile() {
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="file"
-          accept=".jpeg, .jpg, .png"
+          accept="image/*"
           onChange={handleImageChange}
           ref={filePickerRef}
           hidden
